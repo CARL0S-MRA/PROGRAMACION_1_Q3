00ed9e1417b18d11c08a57b426f5af0edcb1f5cf
carlo <carlo@PCdeCarlosM.lan>
2024-07-18 11:56:51 -0600

    Carga Inicial

---
+++ src/programacion_1/DatosAlumnos.java
@@ -0,0 +1,38 @@
+*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programacion_1;
+
+/**
+ *
+ * @author carlo
+ */
+public class DatosAlumnos {
+    public static void main(String []args) {
+        //Comentario de Linea
+        /*
+        Comentario
+        De
+        Bloque
+        */
+        
+        /*
+        Tema: Variable
+        Definicion: Es un contenedor de datos.
+        
+        Tipos de datos 
+        1- Numero
+         1.1 Entero - int, short, long
+         1.2 decimal - double
+        2- Caracter - char
+        3- Cadena de caracteres - String
+        4- Logico - booleanos
+        
+        Sintaxis:
+        Tipo de Datos identificador(Nombre de la varaiable);
+        
+        */
+        
+    }
+}
