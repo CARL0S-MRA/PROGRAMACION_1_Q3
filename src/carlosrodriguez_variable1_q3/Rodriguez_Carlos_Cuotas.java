2eddea49aa270517e3ef4d62103eec2cc065c882
carlo <carlo@PCdeCarlosM>
2024-07-19 09:17:02 -0600

    EJEMPLOS

---
+++ src/carlosrodriguez_variable1_q3/Rodriguez_Carlos_Cuotas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carlosrodriguez_variable1_q3;
+
+/**
+ *
+ * @author carlo
+ */
+public class Rodriguez_Carlos_Cuotas {
+    
+}
