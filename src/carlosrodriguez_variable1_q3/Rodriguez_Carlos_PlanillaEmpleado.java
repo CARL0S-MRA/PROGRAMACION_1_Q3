4e6316f516dba9c0a1523423067c7091b7ff1f73
carlo <carlo@PCdeCarlosM>
2024-07-19 08:48:55 -0600

    EJEMPLOS

---
+++ src/carlosrodriguez_variable1_q3/Rodriguez_Carlos_PlanillaEmpleado.java
@@ -0,0 +1,29 @@
+package carlosrodriguez_variable1_q3;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author carlo
+ */
+import java.util.Scanner;
+public class Rodriguez_Carlos_PlanillaEmpleado {
+    public static void main(String[] args){
+        //Plantilla de Ingreso de Informacion
+        //----- Boleta del Empleado -------
+        //Nombre del Empleado: " "
+        //Hora de Trabajo Mensual: ##
+        //Tarifa por Hora : Lps. ##.##
+        //Salario del Empleado Semanal: Lps. ##.##
+        Scanner lea=new Scanner(System.in);
+        System.out.println("Ingrese el nombre del empleado");
+        String nombrePersona=lea.next();
+        System.out.println("Ingrese numero de horas de trabajo");
+        int horasMensual=lea.nextInt();
+        
+        
+    }
+    
+}
