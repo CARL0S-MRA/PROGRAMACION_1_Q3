6e8cc43b8ece8f7dd31cb1dc9de5b96eec90c6a6
carlo <carlo@PCdeCarlosM>
2024-07-19 09:54:26 -0600

    EJEMPLOS

--- src/carlosrodriguez_variable1_q3/Rodriguez_Carlos_Cuotas.java
+++ src/carlosrodriguez_variable1_q3/Rodriguez_Carlos_Cuotas.java
@@ -1,13 +1,31 @@
+package carlosrodriguez_variable1_q3;
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package carlosrodriguez_variable1_q3;
+import java.util.Scanner;
 
 /**
  *
  * @author carlo
  */
 public class Rodriguez_Carlos_Cuotas {
+    public static void main(String[] args){
+     //PLantilla de Calculacion de Cuota
+//**** CUOTAS MENSUALES *******
+//Cuota de Pago Mensual: HNL ###.##
+//Total a Pagar: HNL ###.##
+
+Scanner lea=new Scanner(System.in);
+System.out.println("Ingresar monto");
+double montoprestamo=lea.nextDouble();
+System.out.println("Ingresar el plazo de meses");
+int plazoMeses =lea.nextInt();
+System.out.println("Ingresar el interes");
+double interes =lea.nextDouble()/100;
+System.out.println("Ingresar");
+    }
     
 }
