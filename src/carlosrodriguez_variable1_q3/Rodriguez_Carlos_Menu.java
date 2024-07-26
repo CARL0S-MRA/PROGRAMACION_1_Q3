b65fd467eeefe7ff5554a8f2d5c1804690bc43e9
carlo <carlo@PCdeCarlosM>
2024-07-26 09:02:12 -0600

    SE CREA EL MENU

---
+++ src/carlosrodriguez_variable1_q3/Rodriguez_Carlos_Menu.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carlosrodriguez_variable1_q3;
+import java.util.Scanner;
+public class Rodriguez_Carlos_Menu {
+    public static void main(String[]args){
+        int opcion=0;
+        
+        while(opcion !=4){
+            System.out.println("----MENU----"+
+                    "\n 1 - CADENA"+
+                    "\n 2 - NOTAS"+
+                    "\n 3 - CLASE"+
+                    "\n 4 - SALIR");
+        }
+    }
+}
+        
